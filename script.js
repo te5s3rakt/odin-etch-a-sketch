54cd109eeaf9450e348f5259cff9896bf4dce877
te5s3rakt <117922694+te5s3rakt@users.noreply.github.com>
2024-10-08 21:56:58 +0800

    Add basic Etch-a-Sketch toy background

---
+++ script.js
@@ -0,0 +1,18 @@
+// const content = document.querySelector('.content');
+
+// function grid(count) {
+//     const grid = document.createElement('div');
+
+//     grid.classList.add('grid');
+    
+//     for (let i = 0; i < count; i++) {
+//         const newCell = document.createElement('div');
+//         newCell.classList.add('grid-cell')
+//         newCell.textContent = i + 1;
+//         grid.appendChild(newCell);
+//     };
+    
+//     return grid;
+// };
+
+// content.appendChild(grid(16 * 16));
