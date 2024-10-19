2bb61770a0459e95b73bcac58dd0c4180a5db2e1
te5s3rakt <117922694+te5s3rakt@users.noreply.github.com>
2024-10-19 19:12:58 +0800

    Add script to screen change buttons

--- script.js
+++ script.js
@@ -16,3 +16,56 @@
 // };
 
 // content.appendChild(grid(16 * 16));
+
+const screenButtons = document.querySelectorAll('button.screen-adjust');
+
+screenButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        changeScreenSize(button);
+        });
+    });
+
+function changeScreenSize (button) {
+    const upperLimit = 100;
+    const lowerLimit = 16;
+    const screenSizeStep = 4;
+
+    const screenSize = document.querySelector('#screen-size');
+    const increaseBtn = document.querySelector('#screen-increase');
+    const decreaseBtn = document.querySelector('#screen-decrease');
+
+    let currentSize = parseInt(screenSize.textContent);
+    
+    if (button.id == 'screen-increase') {
+        if (currentSize == upperLimit) return;
+        
+        let newSize = screenSize.textContent = currentSize + screenSizeStep;
+
+        if (newSize == upperLimit) {
+            button.classList.remove('screen-btn-enabled');
+            button.classList.add('screen-btn-disabled');
+        };
+
+        if (newSize > lowerLimit) {
+            decreaseBtn.classList.remove('screen-btn-disabled');
+            decreaseBtn.classList.add('screen-btn-enabled');
+        };
+    };
+
+    if (button.id == 'screen-decrease') {
+        if (currentSize == lowerLimit) return;
+        
+        let newSize = screenSize.textContent = currentSize - screenSizeStep;
+
+        if (newSize == lowerLimit) {
+            button.classList.remove('screen-btn-enabled');
+            button.classList.add('screen-btn-disabled');
+        };
+
+        if (newSize < upperLimit) {
+            increaseBtn.classList.remove('screen-btn-disabled');
+            increaseBtn.classList.add('screen-btn-enabled');
+        };
+    };
+};
+
